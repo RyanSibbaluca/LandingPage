040eb06483d4c7394c61033e2cc9ab261dab97ee
Ryan Sibbaluca <ryan.sibbaluca@gmail.com>
2017-10-04 17:12:24 +0800

    Add styles on work experience

--- src/app/skills.js
+++ src/app/skills.js
@@ -4,13 +4,20 @@ import WorkExperienceList from './work-experience.json';
 const styles = {
   workExperience: {
     borderBottom: '1px solid #dfdfdf'
+  },
+  columns2: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  marginRight50: {
+    marginRight: '50px'
   }
 };
 
 export class Skills extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {workExperience: []};
   }
 
   componentDidMount() {
@@ -49,16 +56,18 @@ export class Skills extends Component {
           </li>
         </ul>
 
-        <h1>Work Experience</h1>
+        <div style={styles.columns2}>
+          <h1 style={styles.marginRight50}>Work Experience</h1>
 
-        <div>
-          {this.state.workExperience.map(object =>
-            <div style={styles.workExperience} key={object.company + object.role}>
-              <h2>{object.company}</h2>
-              <h3>{object.role}</h3>
-              <p>{object.duration} | {object.location}</p>
-            </div>
-          )}
+          <div>
+            {this.state.workExperience.map(object =>
+              <div style={styles.workExperience} key={object.company + object.role}>
+                <h2>{object.company}</h2>
+                <h3>{object.role}</h3>
+                <p>{object.duration} | {object.location}</p>
+              </div>
+            )}
+          </div>
         </div>
       </section>
     );
