6dc1e70c4cbb94736044e40960efad16fbabc55a
Ryan Sibbaluca <ryan.sibbaluca@gmail.com>
2017-10-04 13:42:11 +0800

    Updated summary section

--- src/app/summary.js
+++ src/app/summary.js
@@ -2,7 +2,13 @@ import React, {Component} from 'react';
 
 const styles = {
   section: {
-    background: '#34e39a'
+    background: '#34e39a',
+    textAlign: 'center',
+    padding: '2rem 0'
+  },
+  container: {
+    maxWidth: '960px',
+    margin: '0 auto'
   }
 };
 
@@ -10,26 +16,30 @@ export class Summary extends Component {
   render() {
     return (
       <section id="summary" style={styles.section}>
-        <h1 className="animate-enter-text">
-          About
-        </h1>
+        <div style={styles.container}>
+          <h1 className="animate-enter-text">
+            Hi Fam! {'I\'m'} Ryan Sibbaluca.
+          </h1>
 
-        <p>
-          I do this, I do that.
-        </p>
-        <p>
-          {'I\'m'} a full-stack web developer based in Singapore.
-        </p>
-        <p>
-          Colin Grist is a freelance designer and developer and for the past eight years he`s worked for some of the UK`s best digital agencies; producing award–winning work wherever he`s been.
-        </p>
-        <p>
-          Working at the very highest level as both a senior developer and senior designer, Colin provides his experience, knowledge and expertise to clients offering them something more than your average freelancer.
-        </p>
+          <p>
+            {'I\'m'} a full-stack web developer based in Singapore.
+          </p>
+          <p>
+            I have 5 years of full-stack software engineering experience.
+            I specialize in front end development and I am very keen in producing great products. I stay on top of my work and I deliver more than what is expected of me.
+          </p>
+          <p>
+            Self-managing, accountable, team player.
+          </p>
+          <p>
+            I love living a holistic life. Playing Basketball, Video games, Board games. TV series - the Flash, Silicon Valley. Working out, eating out, going out of country.
+            Listening to almost all genres - Paramore, John Mayer, Chance the Rapper, Coldplay, Anderson .Paak.
+          </p>
 
-        <h1>
-          I love learning. Life is a forever learning process.
-        </h1>
+          <h1>
+            I love learning. Life is a forever learning process.
+          </h1>
+        </div>
       </section>
     );
   }
